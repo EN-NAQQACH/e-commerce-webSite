78c7ea0ff01252cf64deea65b768303354f6dd0c
EN-NAQQACH <ennaqqach.contact@gmail.com>
2024-08-09 22:17:59 +0100

    wix headless cmc

---
+++ src/context/wixContext.tsx
@@ -0,0 +1,38 @@
+"use client"
+import { createClient, OAuthStrategy } from "@wix/sdk";
+import { products,collections } from "@wix/stores";
+import Cookies from "js-cookie"
+import { createContext } from "react";
+
+
+const refreshToken = JSON.parse(Cookies.get('refreshToken') || "{}");
+
+
+const myWixClient = createClient({
+    modules: {
+      products,
+      collections
+    },
+    auth: OAuthStrategy({
+      clientId: process.env.CLIENT_ID!,
+      tokens: {
+        refreshToken,
+        accessToken : {
+          value: "",
+          expiresAt: 0,
+        }
+        }
+      })
+  })
+  
+export type MyWixClient = typeof myWixClient;
+
+export const wixClientContext = createContext<MyWixClient>(myWixClient);
+
+export const WixClientContextProvider = ({ children }: { children: React.ReactNode }) => {
+    return (
+      <wixClientContext.Provider value={myWixClient}>
+        {children}
+      </wixClientContext.Provider>
+    );
+};

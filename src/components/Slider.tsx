f44f689e096a24833fecd11f400863ef3334d960
EN-NAQQACH <ennaqqach.contact@gmail.com>
2024-08-10 18:49:45 +0100

    Sliders v2

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,69 +1,86 @@
 "use client"
-import React from 'react'
+import React, { useEffect } from 'react'
 const slides = [
     {
         id: 1,
         title: "Summer Sale Collections",
-        description: "Sale! Up to 50% off!",
-        img: "https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&w=800",
+        description: "Find out best spring collection. Offering our best quality production in a X Spring Collection Offering our best quality production in a X Spring Collection",
+        img: "https://uk.lindafarrow.com/cdn/shop/files/IMAGE_6_1920_X_1080_1920x.png?v=1723040224",
         url: "/",
-        bg: "bg-gradient-to-r from-yellow-50 to-pink-50",
     },
     {
         id: 2,
         title: "Winter Sale Collections",
-        description: "Sale! Up to 50% off!",
-        img: "https://images.pexels.com/photos/1021693/pexels-photo-1021693.jpeg?auto=compress&cs=tinysrgb&w=800",
+        description: "Offering our best quality production in a X Spring Collection",
+        img: "https://cdn.prod.website-files.com/66628e28a5c8866918e2d848/666294f9b037a27ae7d30ed4_Wide%20Gallery%201-min-p-1600.jpg",
         url: "/",
-        bg: "bg-gradient-to-r from-pink-50 to-blue-50",
-    },
-    {
-        id: 3,
-        title: "Spring Sale Collections",
-        description: "Sale! Up to 50% off!",
-        img: "https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=800",
-        url: "/",
-        bg: "bg-gradient-to-r from-blue-50 to-yellow-50",
-    },
+    }
 ];
 function Slider() {
     const [currentIndex, setCurrentIndex] = React.useState(0);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
+        }, 5000);
+
+        return () => clearInterval(interval);
+    }, []);
     return (
-        <div className='h-[calc(100vh-70px)] w-full overflow-hidden'>
+        // <div className='h-[calc(100vh-70px)] w-full overflow-hidden'>
+        //     <div className="w-max h-full flex transition-all ease-in-out duration-1000" style={{ transform: `translateX(-${currentIndex * 100}vw)` }}>
+        //         {slides.map(slide => (
+        //             <div className={`${slide.bg} w-screen h-full flex flex-col gap-7 md:flex-row`} key={slide.id}>
+        //                 <div className='TEXT-CONTAINER md:w-1/2 h-full flex justify-center gap-5 items-center flex-col'>
+        //                     <p className='md:text-[20px] lg:text-[25px]'>{slide.title}</p>
+        //                     <p className='md:text-[35px] lg:text-[50px] font-bold'>{slide.description}</p>
+
+        //                     <button className='p-2 border bg-black text-white rounded-sm'>Shop Now</button>
+        //                 </div>
+        //                 <div className='IMAGE-CONTAINER  md:w-1/2 h-1/2  '>
+        //                     <img src={slide.img} alt="" sizes='100%' className='object-cover' />
+        //                 </div>
+        //             </div>
+        //         ))}
+        //     </div>
+        //     <div className='absolute bottom-5 w-full flex justify-center gap-2'>
+        //         {slides.map((slide, index) => (
+        //             <div className={`w-3 h-3  rounded-full ring-1 ring-black cursor-pointer flex items-center justify-center ${currentIndex === index ? "scale-150" : ""
+        //                 }`}
+        //                 key={slide.id}
+        //                 onClick={() => setCurrentIndex(index)}
+        //             >
+        //                 {currentIndex === index && (
+        //                     <div className="w-[6px] h-[6px] bg-black rounded-full"></div>
+        //                 )}
+
+        //             </div>
+        //         ))}
+        //     </div>
+
+        // </div>
+        <div className='px-14'>
+        <div className='rounded-2xl relative h-[88vh] w-[100%] overflow-hidden'>
             <div
-        className="w-max h-full flex transition-all ease-in-out duration-1000"
-        style={{ transform: `translateX(-${currentIndex * 100}vw)` }}
-      >
-                {slides.map(slide => (
-                    <div className={`${slide.bg} w-screen h-full flex flex-col gap-7 md:flex-row`} key={slide.id}>
-                        <div className='TEXT-CONTAINER md:w-1/2 h-full flex justify-center gap-5 items-center flex-col'>
-                        <p className='md:text-[20px] lg:text-[25px]'>{slide.title}</p>
-                            <p className='md:text-[35px] lg:text-[50px] font-bold'>{slide.description}</p>
-                            
-                            <button className='p-2 border bg-black text-white rounded-sm'>Shop Now</button>
-                        </div>
-                        <div className='IMAGE-CONTAINER  md:w-1/2 h-1/2  '>
-                            <img src={slide.img} alt="" sizes='100%' className='object-cover' />
+                className='flex transition-transform duration-500 ease-in-out'
+                style={{ transform: `translateX(-${currentIndex * 100}vw)` }}
+            >
+                {slides.map((slide) => (
+                    <div key={slide.id} className='w-[100vw] h-full flex-shrink-0'>
+                        <img src={slide.img} alt={slide.title} className='w-full h-[88vh] object-cover' />
+                        <div className='absolute bottom-4 text-white flex flex-col gap-8 px-4 w-[100%]'>
+                            <div>
+                                <h1 className='text-5xl font-bold'>{slide.title}</h1>
+                            </div>
+                            <div className='flex justify-between items-center'>
+                                <p className='w-[70%] text-[14px]'>{slide.description}</p>
+                                <button className='h-fit w-fit text-[14px] bg-white text-black rounded-3xl px-6 py-2 transition-all duration-700 ease-in-out hover:shadow-md'>Shop Now</button>
+                            </div>
                         </div>
                     </div>
                 ))}
             </div>
-            <div className='absolute bottom-5 w-full flex justify-center gap-2'>
-                {slides.map((slide, index) => (
-                    <div className={`w-3 h-3  rounded-full ring-1 ring-black cursor-pointer flex items-center justify-center ${currentIndex === index ? "scale-150" : ""
-                        }`}
-                        key={slide.id}
-                        onClick={() => setCurrentIndex(index)}
-                        >
-                        {currentIndex === index && (
-                            <div className="w-[6px] h-[6px] bg-black rounded-full"></div>
-                        )}
-
-                    </div>
-                ))}
-            </div>
-
         </div>
+    </div>
     )
 }
 

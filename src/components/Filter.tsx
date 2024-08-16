0e9a45d23d4402ef532c2ef37c06b31e194e5327
EN-NAQQACH <ennaqqach.contact@gmail.com>
2024-08-16 23:17:52 +0100

    filter funcionality

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -2,27 +2,43 @@
 import React, { useState } from 'react'
 import { Button, Modal } from 'antd';
 import { VscSettings } from "react-icons/vsc";
+import { useParams, usePathname, useSearchParams, useRouter } from 'next/navigation';
+import { collections } from '@wix/stores';
 
-const Filter = () => {
+const Filter = ({ gategories }: { gategories: collections.Collection[] }) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const showModal = () => {
         setIsModalOpen(true);
     };
-
     const handleOk = () => {
         setIsModalOpen(false);
     };
-
     const handleCancel = () => {
         setIsModalOpen(false);
     };
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+    const params = new URLSearchParams(searchParams);
+    const { replace } = useRouter();
+    const handleFilter = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement >) => {
+        const { name, value } = e.target;
+        console.log(name, value);
+        const params = new URLSearchParams(searchParams);
+        params.set(name, value);
+        replace(`${pathname}?${params.toString()}`);
+    }
+    const handleButtonsFilter = (e: React.MouseEvent<HTMLButtonElement>) => {
+        const { value } = e.currentTarget;
+        const params = new URLSearchParams(searchParams);
+        params.set('categorie', value);
+        replace(`${pathname}?${params.toString()}`);
+    }
     return (
         <div>
             <div className='flex items-center gap-3'>
-                <button className='px-7 py-1 border border-black rounded-2xl hover:bg-black hover:text-white'>T-shirt</button>
-                <button className='px-7 py-1 border border-black rounded-2xl hover:bg-black hover:text-white'>Jacket</button>
-                <button className='px-7 py-1 border border-black rounded-2xl hover:bg-black hover:text-white'>Pants</button>
-                <button className='px-7 py-1 border border-black rounded-2xl hover:bg-black hover:text-white'>Hoodie</button>
+                {gategories.map((cat)=>{
+                    return <button value={cat.slug!} key={cat._id} className='px-7 py-1 border border-black rounded-2xl hover:bg-black hover:text-white' onClick={handleButtonsFilter}>{cat.name}</button>
+                })}
                 <button className='px-[10px] py-[10px] border border-black rounded-[100%] hover:bg-black hover:text-white' onClick={showModal}>
                     <VscSettings className='' />
                 </button>
@@ -32,39 +48,32 @@ const Filter = () => {
                 }} className='rounded-none ' title="FILTER" open={isModalOpen} footer={false} onCancel={handleCancel}>
                     <div className='footer-content flex flex-col gap-3'>
                         <div>
-                            <input type="text" className='p-2 outline-none w-full rounded-md border' placeholder='search product' />
+                            <input type="text" name='search' className='p-2 outline-none w-full rounded-md border' placeholder='search product' onChange={handleFilter} />
                         </div>
                         <div>
                             <label htmlFor="" className='font-semibold '>By Categorie</label>
-                            <select name="" id="" className='w-full p-2 outline-none rounded-2xl border border-black mt-2'>
-                                <option value="">T-shirt</option>
-                                <option value="">Jacket</option>
-                                <option value="">Pants</option>
-                                <option value="">Hoodie</option>
+                            <select name="categorie" id="" onChange={handleFilter} className='w-full p-2 outline-none rounded-2xl border border-black mt-2' >
+                                <option value="t-shirt">T-shirt</option>
+                                <option value="jacket">Jacket</option>
+                                <option value="pants">Pants</option>
+                                <option value="hoodie">Hoodie</option>
                             </select>
                         </div>
                         <div>
                             <label htmlFor="" className='font-semibold'>Sort By</label>
-                            <select name="" id="" className='w-full p-2 outline-none rounded-2xl border border-black mt-2'>
-                                <option value="">Price</option>
-                                <option value="">Rating</option>
-                                <option value="">Latest</option>
+                            <select name="sortby" id="" onChange={handleFilter} className='w-full p-2 outline-none rounded-2xl border border-black mt-2'>
+                                <option value="desc price">Price (high to low)</option>
+                                <option value="asc price">Price (low to high)</option>
+                                <option value="asc lastUpdated">Newest</option>
+                                <option value="desc lastUpdated">Oldest</option>
                             </select>
                         </div>
-                        <div>
-                            <label htmlFor="" className='font-semibold'>Color</label>
-                            <select name="" id="" className='w-full p-2 outline-none rounded-2xl border border-black mt-2'>
-                                <option value="">Red</option>
-                                <option value="">Blue</option>
-                                <option value="">Green</option>
-                                <option value="">Yellow</option>
-                            </select>
-                        </div>
+
                         <div>
                             <label htmlFor="" className='font-semibold'>Price</label>
                             <div className='flex items-center justify-center gap-5'>
-                                <input type="number" name="" id="" className='w-full rounded-2xl border  border-black p-2' min={1} placeholder='min price' />
-                                <input type="number" name="" id="" className='w-full rounded-2xl border  border-black p-2' min={1} placeholder='max price' />
+                                <input type="number" name="minprice" id="" className='w-full rounded-2xl border  border-black p-2' min={1} placeholder='min price' onChange={handleFilter} />
+                                <input type="number" name="maxprice" id="" className='w-full rounded-2xl border  border-black p-2' min={1} placeholder='max price' onChange={handleFilter} />
                             </div>
                         </div>
                         <div className='mt-2'>
--- src/components/ListPage.tsx
+++ src/components/ListPage.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import { wixClientServer } from "@/lib/wixClientServer";
-
+import DOMPurify from 'isomorphic-dompurify';
 
 const ListPage = async ({ searchParams, categorieId }: { searchParams: any, categorieId: any }) => {
   const wixClient = await wixClientServer();
-  const res = await wixClient.products.queryProducts().eq(
+  const Query = wixClient.products.queryProducts().eq(
     "collectionIds", categorieId
-  ).limit(9).find();
+  )
+    .startsWith("name", searchParams?.search || "")
+    .ge("priceData.price", searchParams.minprice || 0)
+    .le("priceData.price", searchParams.maxprice || 99999)
+    .limit(9)
+
+  if(searchParams?.sortby){
+    const [sortType, sort] = searchParams.sortby.split(" ");
+    if(sortType === "asc"){
+      Query.ascending(sort)
+    }
+    if(sortType === "desc"){
+      Query.descending(sort)
+    }
+  }
+  const res = await Query.find();
   return (
     <div className=' w-full min-h-screen py-2 lg:px-2 md:px-5 sm:px-10 px-20  mt-3'>
       <div className='container grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-3 min-w-[100%]  '>
@@ -15,7 +30,7 @@ const ListPage = async ({ searchParams, categorieId }: { searchParams: any, cate
           <div className='product-content   ' key={item._id}>
             <div className='relative'>
               <div className='h-[400px]'>
-                <img src="https://www.mrporter.com/variants/images/1647597343941045/in/w560_q60.jpg" alt="" className='rounded-sm h-full object-cover w-full' />
+                <img src={item.media?.mainMedia?.image?.url} alt="" className='rounded-sm h-full object-cover w-full' />
               </div>
               <div className='absolute bg-[#ffffff4d] flex justify-center items-center transition-all duration-300 w-full h-full bottom-0 opacity-0 hover:opacity-100 text-white px-2 py-1 rounded-lg'>
 
@@ -24,10 +39,17 @@ const ListPage = async ({ searchParams, categorieId }: { searchParams: any, cate
               </div>
             </div>
             <div className='mt-2 flex flex-col gap-1'>
-              <p className='font-bold'>Loro piano</p>
-              <p className='text-[12px] text-gray-400'>Slim short this is nice shirt for you</p>
-              <p className='font-bold'>$45</p>
-            </div>
+                <p className='font-bold overflow-hidden text-ellipsis whitespace-nowrap'>
+                  {item.name}
+                </p>              
+                <div className='text-[12px] text-gray-400 h-[25px] overflow-hidden text-ellipsis whitespace-nowrap'
+                  dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(item.additionalInfoSections?.find((section:any) => section.title ="shortDescription")?.description || ""),
+                  }}
+                >
+                </div>
+                <p className='font-bold'>${item.price?.price}</p>
+              </div>
           </div>
         ))}
       </div>

33fa295b4db929b42ecbed1d097fbd49d6f05518
EN-NAQQACH <ennaqqach.contact@gmail.com>
2024-08-17 20:48:23 +0100

    Add font

---
+++ src/font/font.tsx
@@ -0,0 +1,12 @@
+import {Rajdhani} from 'next/font/google'
+export const rajdhani = Rajdhani({
+    subsets: ['latin'],
+    weight: '500',
+    display: 'swap'
+})
+export const rajdhani2 = Rajdhani({
+    subsets: ['latin'],
+    weight: '400',
+    display: 'swap'
+})
+

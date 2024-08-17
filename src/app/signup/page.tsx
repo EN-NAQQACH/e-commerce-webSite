138667fb51857ec1a47f15b1703a2e1954776075
EN-NAQQACH <ennaqqach.contact@gmail.com>
2024-08-17 20:50:04 +0100

    login / register page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -13,7 +13,7 @@ function page() {
               <p className='text-[35px]'>Hello, Welcome Back</p>
               <p className='text-[12px] text-gray-500'>Complete  the data below to enjoy our services</p>
             </div>
-            <div className='flex flex-col gap-14'>
+            <div className='flex flex-col gap-10'>
               <div className='flex flex-col gap-4'>
                 <div>
                   <label htmlFor="" className='text-[15px] font-[500] '>Email or Username</label>
@@ -23,12 +23,12 @@ function page() {
                   <label htmlFor="" className='text-[15px] font-[500] '>Password</label>
                   <input type="password" className='w-full border p-2 rounded-3xl mt-1 text-[12px] pl-5' placeholder='Enter your password' />
                 </div>
-                
-                <div className='flex justify-end text-[12px] font-[550]'>
-                  <Link href={"/forget"} className='hover:underline'>Forgot password</Link>
+                <div>
+                  <label htmlFor="" className='text-[15px] font-[500] '>Password</label>
+                  <input type="password" className='w-full border p-2 rounded-3xl mt-1 text-[12px] pl-5' placeholder='Enter your password' />
                 </div>
                 <div className='flex gap-3'>
-                  <button className='w-full border rounded-3xl bg-black text-white py-[6px]'>Sign in</button>
+                  <button className='w-full border rounded-3xl bg-black text-white py-[6px]'>Sign up</button>
                   {/* <button className='flex items-center gap-2 w-1/2 border rounded-3xl py-[6px] justify-center'>
                     <img src="/Google.png" alt="" className='w-[20px] h-[20px]' />
                     <p className='text-[14px]'>Sign in with Google</p>
@@ -36,7 +36,7 @@ function page() {
                 </div>
               </div>
               <div className=''>
-                <p className='text-[13px] font-[500] text-gray-500'>Don't have an account? <Link href={"/signup"} className='text-black font-bold'>Sign up</Link></p>
+                <p className='text-[13px] font-[500] text-gray-500'>Already have an account? <Link href={"/login"} className='text-black font-bold'>Sign in</Link></p>
               </div>
             </div>
           </div>
